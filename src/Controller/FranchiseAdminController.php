974467a5337d7521fb06c4d4d4872c73335b87e4
Thomas GEOFFRON <tgeoffron@myges.fr>
2021-12-04 16:03:02 +0100

    feat: Setup sidebar for Franchise Admin

--- src/Controller/FranchiseAdminController.php
+++ src/Controller/FranchiseAdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,15 @@ class FranchiseAdminController extends AbstractController
     #[Route('/franchise/admin', name: 'franchise_admin')]
     public function index(): Response
     {
-        return $this->render('franchise_admin/index.html.twig', [
-            'controller_name' => 'FranchiseAdminController',
-        ]);
+        $user = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findBy(['id' => $this->get('session')->get('user')]);
+        if ($user[0]->getRole() === 3) {
+            return $this->render('franchise_admin/index.html.twig', [
+                'user' => $user[0],
+            ]);
+        } else {
+            return $this->redirectToRoute('accueil');
+        }
     }
 }

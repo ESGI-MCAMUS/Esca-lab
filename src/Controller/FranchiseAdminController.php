c76be510e01195969d8dfe1d2c5643ebe878d2af
Thomas GEOFFRON <tgeoffron@myges.fr>
2022-01-08 19:13:36 +0100

    feat: Add Franchise Admin template page

--- src/Controller/FranchiseAdminController.php
+++ src/Controller/FranchiseAdminController.php
@@ -6,18 +6,29 @@ use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+use DateTime;
 
 class FranchiseAdminController extends AbstractController
 {
+    private $user;
+
+    public function __construct(Security $security)
+    {
+        $this->user = $security->getUser();
+    }
+
     #[Route('/franchise/admin', name: 'franchise_admin')]
     public function index(): Response
     {
-        $user = $this->getDoctrine()
-            ->getRepository(User::class)
-            ->findBy(['id' => $this->get('session')->get('user')]);
-        if ($user[0]->getRole() === 3) {
-            return $this->render('franchise_admin/index.html.twig', [
-                'user' => $user[0],
+        dump($this->user ? $this->user->getRoles() : 'null_user');
+        if (
+            $this->user
+                ? in_array("ROLE_ADMIN_FRANCHISE", $this->user->getRoles())
+                : false
+        ) {
+            return $this->render('franchise/index.html.twig', [
+                'month' => date_format(new DateTime(), 'n'),
             ]);
         } else {
             return $this->redirectToRoute('accueil');

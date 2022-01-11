4846dc70308e7bf0e45a6028eb81913ccff04116
Thomas GEOFFRON <tgeoffron@myges.fr>
2022-01-11 15:29:02 +0100

    Feat: Modification and deletion of employees + Security modifictions

--- src/Controller/FranchiseAdminController.php
+++ src/Controller/FranchiseAdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,6 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
 use DateTime;
 
+#[IsGranted('ROLE_ADMIN_FRANCHISE')]
 class FranchiseAdminController extends AbstractController
 {
     private $user;
@@ -49,6 +51,77 @@ class FranchiseAdminController extends AbstractController
             return $this->render('franchise/employees.html.twig', [
                 'employees' => $employees,
             ]);
+        } else {
+            return $this->redirectToRoute('accueil');
+        }
+    }
+
+    #[Route('/franchise/employees/edit/{id}/{check}', name: 'edit_employee')]
+    public function editEmployee($id, $check = 'user') {
+        if (
+            $this->user
+                ? in_array("ROLE_ADMIN_FRANCHISE", $this->user->getRoles())
+                : false
+        ) {
+            $repo = $this->getDoctrine()->getManager()->getRepository(User::class);
+            $employee = $repo->find($id);
+
+            if ($employee->getFranchise()->getId() === $this->user->getFranchise()->getId()) {
+                $roles = $employee->getRoles();
+
+                switch ($check) {
+                    case 'ouvreur':
+                        $key = array_search("ROLE_OUVREUR", $roles);
+                        if ($key) $employee->setRoles(["ROLE_USER"]);
+                        else $employee->setRoles(["ROLE_OUVREUR"]);
+                        break;
+                    case 'admin_franchise':
+                        $key = array_search("ROLE_ADMIN_FRANCHISE", $roles);
+                        if ($key) $employee->setRoles(["ROLE_USER"]);
+                        else $employee->setRoles(["ROLE_ADMIN_FRANCHISE"]);
+                        break;
+                    case 'admin_salle':
+                        $key = array_search("ROLE_ADMIN_SALLE", $roles);
+                        if ($key) $employee->setRoles(["ROLE_USER"]);
+                        else $employee->setRoles(["ROLE_ADMIN_SALLE"]);
+                        break;
+                    default:
+                        return $this->redirectToRoute('franchise_employees');
+                }
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($employee);
+                $em->flush();
+            }
+
+            return $this->redirectToRoute('franchise_employees');
+        } else {
+            return $this->redirectToRoute('accueil');
+        }
+    }
+
+    #[Route('/franchise/employees/remove/{id}', name: 'remove_employee')]
+    public function removeEmployee($id) {
+        if (
+            $this->user
+                ? in_array("ROLE_ADMIN_FRANCHISE", $this->user->getRoles())
+                : false
+        ) {
+            $repo = $this->getDoctrine()->getManager()->getRepository(User::class);
+            $employee = $repo->find($id);
+
+            dump($employee);
+
+            if ($employee->getFranchise()->getId() === $this->user->getFranchise()->getId()) {
+                $employee->setRoles(["ROLE_USER"]);
+                $employee->setFranchise(null);
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($employee);
+                $em->flush();
+            }
+
+            return $this->redirectToRoute('franchise_employees');
         } else {
             return $this->redirectToRoute('accueil');
         }

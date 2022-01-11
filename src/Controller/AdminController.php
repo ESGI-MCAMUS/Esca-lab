787dfaeb7ee8f4a69bd4d944d41b96861f83b7fb
Milan Camus <pro@turtletv.fr>
2022-01-11 15:17:40 +0100

    feat: ✨ Add user search bar and logic

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -10,13 +10,23 @@ use App\Entity\User;
 use DateTime;
 use Symfony\Component\Security\Core\Security;
 
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Form\UserSearchType;
+
+use Doctrine\ORM\EntityManagerInterface;
+
 class AdminController extends AbstractController
 {
     private $user;
+    private $em;
 
-    public function __construct(Security $security)
-    {
+    public function __construct(
+        Security $security,
+        EntityManagerInterface $entityManager
+    ) {
         $this->user = $security->getUser();
+        $this->em = $entityManager;
     }
 
     #[Route('/admin', name: 'admin')]
@@ -29,14 +39,28 @@ class AdminController extends AbstractController
     }
 
     #[Route('/admin/utilisateurs', name: 'admin_users')]
-    public function admin_users(): Response
+    public function admin_users(Request $request): Response
     {
-        dump($this->user ? $this->user->getRoles() : 'null_user');
+        $form = $this->createForm(UserSearchType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $search = $form->get('search')->getData();
+            $userRepo = $this->getDoctrine()
+                ->getManager()
+                ->getRepository(User::class);
+            $result = $userRepo->search('%' . $search . '%');
+            return $this->renderForm('admin/users.html.twig', [
+                'users' => $result,
+                'form' => $form,
+            ]);
+        }
+
         $entityManager = $this->getDoctrine()->getManager();
         $users = $entityManager->getRepository(User::class)->findAll();
-        dump($users);
-        return $this->render('admin/users.html.twig', [
+        return $this->renderForm('admin/users.html.twig', [
             'users' => $users,
+            'form' => $form,
         ]);
     }
 }

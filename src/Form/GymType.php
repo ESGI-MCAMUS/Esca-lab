cfd5db620bda99a8d1f11f1e805e133e3182f052
Milan Camus <pro@turtletv.fr>
2022-03-05 17:22:25 +0100

    feat: 💄 Style enhancement for `Gym` form

--- src/Form/GymType.php
+++ src/Form/GymType.php
@@ -8,21 +8,60 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+
 class GymType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
+    public function buildForm(
+        FormBuilderInterface $builder,
+        array $options
+    ): void {
         $builder
-            ->add('name')
-            ->add('size')
-            ->add('pc')
-            ->add('address')
-            ->add('city')
-            ->add('save', SubmitType::class, [
-                'label' => 'Save !',
-                'attr' => ['class' => "btn btn-primary"]
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la salle',
+                'label_attr' => ['class' => 'form-label poppins light'],
+                'attr' => [
+                    'class' => 'form-control poppins',
+                    'placeholder' => 'Ma Super Salle',
+                ],
             ])
-        ;
+            ->add('size', NumberType::class, [
+                'label' => 'Taille de la salle (en m²)',
+                'label_attr' => ['class' => 'form-label poppins light'],
+                'attr' => [
+                    'class' => 'form-control poppins',
+                    'placeholder' => 150,
+                ],
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Adresse postale de la salle',
+                'label_attr' => ['class' => 'form-label poppins light'],
+                'attr' => [
+                    'class' => 'form-control poppins',
+                    'placeholder' => '38 rue des Jeuneurs',
+                ],
+            ])
+            ->add('pc', NumberType::class, [
+                'label' => 'Code postal de la salle',
+                'label_attr' => ['class' => 'form-label poppins light'],
+                'attr' => [
+                    'class' => 'form-control poppins',
+                    'placeholder' => 75012,
+                ],
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville de la salle',
+                'label_attr' => ['class' => 'form-label poppins light'],
+                'attr' => [
+                    'class' => 'form-control poppins',
+                    'placeholder' => 'Paris',
+                ],
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Ajouter la salle',
+                'attr' => ['class' => 'btn btn-primary'],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void

47197ddf90d72f60c7af186940231ef22078d91e
Milan Camus <pro@turtletv.fr>
2022-06-27 20:01:38 +0200

    fix: 💄 Page style mainly for forms

--- src/Form/EmployeeType.php
+++ src/Form/EmployeeType.php
@@ -8,23 +8,22 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class EmployeeType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
+class EmployeeType extends AbstractType {
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
         $builder
             ->add('username', TextType::class, [
-                'label' => 'Nom de l\'utilisateur',
+                'label' => 'Nom de l\'employé',
+                'label_attr' => ['class' => 'form-label poppins light'],
                 'attr'  => [
-                    'placeholder' => 'Milan le plus bo'
+                    'placeholder' => 'John Doe',
+                    'class' => 'form-control poppins',
+
                 ]
             ])
-            ->add('send', SubmitType::class)
-        ;
+            ->add('send', SubmitType::class, ['attr' => ['class' => 'btn btn-primary'], 'label' => 'Rechercher',]);
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
+    public function configureOptions(OptionsResolver $resolver): void {
         $resolver->setDefaults([
             // Configure your form options here
         ]);

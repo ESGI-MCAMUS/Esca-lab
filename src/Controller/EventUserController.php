67dd7048a808f45122b970f89b1106505414770d
Arthur Vadrot <krafteck666@gmail.com>
2022-07-05 17:50:37 +0200

    Feat : Finished the update of an event + UI/UX improvement

--- src/Controller/EventUserController.php
+++ src/Controller/EventUserController.php
@@ -11,6 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 
 use App\Entity\Event;
+use App\Entity\Gym;
 use App\Entity\Franchise;
 use App\Entity\User;
 use Doctrine\Persistence\ManagerRegistry;
@@ -75,6 +76,59 @@ class EventUserController extends AbstractController
             $entityManager->flush();
         } else {
             $success = false;
+        }
+
+        return new JsonResponse(['success' => $success]);
+    }
+
+    #[IsGranted('ROLE_USER')]
+    #[Route('/event/edit/{eventId}', name: 'event_edit', defaults: ["eventId" => null], methods: ['POST'])]
+    public function editEvent(ManagerRegistry $doctrine, Request $request, $eventId): Response
+    {
+        $success = true;
+        $had_changes = false;
+
+        $entityManager = $doctrine->getManager();
+        $targetEvent = $entityManager
+            ->getRepository(Event::class)
+            ->find($eventId);
+        
+        $ajaxParams = $request->request->all();
+
+        if(strcmp($ajaxParams['titre'], $targetEvent->getTitle()) !== 0) {
+            $had_changes = true;
+            $targetEvent->setTitle($ajaxParams['titre']);
+        }
+
+        $newGym = $entityManager
+            ->getRepository(Gym::class)
+            ->find(intval($ajaxParams['gym']));        
+
+        if($newGym->getId() !== $targetEvent->getGym()->getId()) {
+            $had_changes = true;
+            $targetEvent->setGym($newGym);
+        }
+
+        $startEditedEvent = new \DateTime($ajaxParams['startEvent']);
+        $endEditedEvent = new \DateTime($ajaxParams['endEvent']);
+        
+        if($startEditedEvent !== $targetEvent->getEventDate()) {
+            $had_changes = true;
+            $targetEvent->setEventDate($startEditedEvent);
+        }
+
+        if($endEditedEvent !== $targetEvent->getEndDate()) {
+            $had_changes = true;
+            $targetEvent->setEndDate($endEditedEvent);
+        }
+
+        if(strcmp($ajaxParams['description'], $targetEvent->getDescription()) !== 0) {
+            $had_changes = true;
+            $targetEvent->setDescription($ajaxParams['description']);
+        }
+
+        if($had_changes) {
+            $entityManager->flush();
         }
 
         return new JsonResponse(['success' => $success]);

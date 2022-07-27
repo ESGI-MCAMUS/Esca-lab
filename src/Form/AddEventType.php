6621c20429475763fbe46b0610945a1d12bc1b16
Milan Camus <pro@turtletv.fr>
2022-07-27 19:47:44 +0200

    chore: 🎨 Code format

--- src/Form/AddEventType.php
+++ src/Form/AddEventType.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
@@ -9,55 +10,72 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
-class AddEventType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
+class AddEventType extends AbstractType {
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
         $array_options_gyms = [];
-        foreach ($options['data']['gyms'] as $key => $value){
-            $array_options_gyms[$value->getName()] = "".$value->getId();
+        foreach ($options['data']['gyms'] as $key => $value) {
+            $array_options_gyms[$value->getName()] = "" . $value->getId();
         }
 
         $builder
             ->add('title', ChoiceType::class, [
                 'label' => 'Quel type d\'événement',
+                'label_attr' => ['class' => 'form-label poppins light'],
                 'choices' => [
                     "Compétition" => "competition",
                     "Entraînement" => "entrainement",
                     "Renforcement" => "renforcement",
                     "Yoga" => "yoga",
                 ],
+                'attr' => [
+                    'class' => 'form-control poppins',
+                ],
             ])
-            ->add('description')
+            ->add('description', TextType::class, [
+                'label' => 'Description l\'événement',
+                'label_attr' => ['class' => 'form-label poppins light'],
+                'attr' => [
+                    'class' => 'form-control poppins',
+                    'placeholder' => 'Mon super évènement',
+                    'required' => true,
+                ],
+            ])
             ->add('eventDate', DateTimeType::class, [
                 'widget' => 'single_text',
                 'input' => 'datetime',
+                'label_attr' => ['class' => 'form-label poppins light'],
                 'placeholder' => [
                     'year' => 'Year', 'month' => 'Month', 'day' => 'Day',
                     'hour' => 'Hour', 'minute' => 'Minute',
                 ],
                 'attr' => [
-                    'min' => ( new \DateTime() )->format('Y-m-d H:i')
+                    'min' => (new \DateTime())->format('Y-m-d H:i'),
+                    'class' => 'form-control poppins'
                 ]
             ])
             ->add('endDate', DateTimeType::class, [
                 'widget' => 'single_text',
+                'label_attr' => ['class' => 'form-label poppins light'],
                 'placeholder' => [
                     'year' => 'Year', 'month' => 'Month', 'day' => 'Day',
                     'hour' => 'Hour', 'minute' => 'Minute',
                 ],
                 'attr' => [
-                    'min' => ( new \DateTime() )->format('Y-m-d H:i')
+                    'min' => (new \DateTime())->format('Y-m-d H:i'),
+                    'class' => 'form-control poppins'
                 ]
             ])
             ->add('gymId', ChoiceType::class, [
                 'label' => 'Dans quelle salle',
                 'choices' => $array_options_gyms,
-            ])  
+                'label_attr' => ['class' => 'form-label poppins light'],
+                'attr' => [
+                    'class' => 'form-control poppins',
+                ],
+            ])
             ->add('save', SubmitType::class, [
                 'label' => 'Créer l\'événement',
                 'attr' => ['class' => 'btn btn-primary'],
-            ]);
-        ;
+            ]);;
     }
 }
--- src/Form/AddFriendType.php
+++ src/Form/AddFriendType.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
@@ -7,18 +8,22 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class AddFriendType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
+class AddFriendType extends AbstractType {
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
         $builder
             ->add('username',   TextType::class, [
                 'label' => 'Nom de l\'utilisateur',
+                'label_attr' => ['class' => 'form-label poppins light'],
                 'attr'  => [
-                    'placeholder' => 'Milan le plus bo'
+                    'placeholder' => 'Trouvez l\'utilisateur',
+                    'class' => 'form-control poppins',
                 ]
             ])
-            ->add('send',       SubmitType::class)
-        ;
+            ->add('send', SubmitType::class, [
+                'label' => 'Chercher l\'utilisateur',
+                'attr' => [
+                    'class' => 'btn btn-outline-primary poppins',
+                ],
+            ]);
     }
 }
